9a71ef8817df3d29059b135bdac41b30da90acd1
mic29226 <michael.peters@fokus.fraunhofer.de>
2024-10-10 10:05:49 +0200

    fix: cannot import DPI Home because this is dependend on the Happyflow-Modules-Version.

--- src/router.js
+++ src/router.js
@@ -2,7 +2,7 @@
 import * as Router from 'vue-router';
 import { watch } from 'vue';
 import { glueConfig as GLUE_CONFIG } from '../config/user-config';
-import {DPIHome} from "@piveau/piveau-hub-ui-modules"
+// import {DPIHome} from "@piveau/piveau-hub-ui-modules"
 import {
   Auth,
   DatasetDetailsCategories,
@@ -179,12 +179,12 @@ if (GLUE_CONFIG.content.dataProviderInterface.useService) {
     component: DataFetchingComponent,
     props: true
   }),
-  router.addRoute({
-    path: '/dpi/home',
-    name: "DPI-Home-HappyFlow",
-    component: DPIHome,
-    props: true
-  }),
+  // router.addRoute({
+  //   path: '/dpi/home',
+  //   name: "DPI-Home-HappyFlow",
+  //   component: DPIHome,
+  //   props: true
+  // }),
   router.addRoute({
     path: "/dpi",
     name: "DataProviderInterface",
